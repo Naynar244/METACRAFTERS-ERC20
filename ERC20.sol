4fac71af3a5375ba2cd98dd5a31d43fb082f7752
Naynar244 <naynarjwanjiru@gmail.com>
2024-07-17 19:48:12 +0100

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,25 +1,34 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function transfer(address recipient, uint256 amount) external returns (bool);
-    function mint(address account, uint256 amount) external;
-    function burn(address account, uint256 amount) external;
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-contract ERC20Token is IERC20 {
+contract ERC20 is IERC20 {
     string public name;
     string public symbol;
-    uint8 public decimals = 18;
+    uint8 public decimals;
     uint256 private _totalSupply;
 
     mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
-    constructor(string memory _name, string memory _symbol) {
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 initialSupply) {
         name = _name;
         symbol = _symbol;
+        decimals = _decimals;
+        _totalSupply = initialSupply * 10 ** uint256(decimals);
+        _balances[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
     function totalSupply() public view override returns (uint256) {
@@ -31,25 +40,46 @@ contract ERC20Token is IERC20 {
     }
 
     function transfer(address recipient, uint256 amount) public override returns (bool) {
+        require(recipient != address(0), "ERC20: transfer to the zero address");
         require(_balances[msg.sender] >= amount, "ERC20: transfer amount exceeds balance");
+
         _balances[msg.sender] -= amount;
         _balances[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function mint(address account, uint256 amount) public override {
-        require(account != address(0), "ERC20: mint to the zero address");
-        _totalSupply += amount;
-        _balances[account] += amount;
-        emit Transfer(address(0), account, amount);
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
     }
 
-    function burn(address account, uint256 amount) public override {
-        require(account != address(0), "ERC20: burn from the zero address");
-        require(_balances[account] >= amount, "ERC20: burn amount exceeds balance");
-        _balances[account] -= amount;
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        require(_balances[sender] >= amount, "ERC20: transfer amount exceeds balance");
+        require(_allowances[sender][msg.sender] >= amount, "ERC20: transfer amount exceeds allowance");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        _allowances[sender][msg.sender] -= amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function burn(uint256 amount) public returns (bool) {
+        require(_balances[msg.sender] >= amount, "ERC20: burn amount exceeds balance");
+
+        _balances[msg.sender] -= amount;
         _totalSupply -= amount;
-        emit Transfer(account, address(0), amount);
+        emit Transfer(msg.sender, address(0), amount);
+        return true;
     }
 }
